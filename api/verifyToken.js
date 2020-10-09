568036a91a27b8ea16831e0229d87656d0aed9f9
thanh.nq <momo2941998@gmail.com>
2020-10-09 13:35:20 +0700

    add verify token from req.body.token

---
+++ api/verifyToken.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function(req, res, next) {
+    const token = req.body.token;
+    if(!token) return res.status(401).send({
+        code: 9998,
+        message: "Token is invalid"
+      });
+
+    try {
+        // if decode token success return into req.user
+        const verified = jwt.verify(token, config.get('jwtSecret'));
+        req.user = verified;
+        next();
+    } catch (err) {
+        res.status(400).send({
+            code: 9998,
+            message: "Token is invalid"
+          });
+    }
+}
