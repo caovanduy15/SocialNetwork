8b375ae630984a83425dc3ee5954965714d4d000
unknown <namanhle941998@gmail.com>
2020-10-30 19:07:01 +0700

    Add check new version

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -243,6 +243,28 @@ router.post("/logout", (req, res) => {
 
 router.post("/set_devtoken", (req, res) => {
     var { token, devtype, devtoken} = req.body;
+    if (!token || !devtype || !devtoken)
+        return res.status(400).json({ code: 1004, message: "Please enter all fields" });
+    jwt.verify(token, config.get('jwtSecret'), (err, user) => {
+
+      // not valid token
+      if (("undefined" === typeof (user))) {
+        return res.json({ code: 1004, message: "Invalid token" });
+      }
+      if (user.isBlocked){
+          return res.json({ code: 1004, message: "Your account is blocked" });
+      }
+    });
+    if (devtype != "Android" && devtype != "IOS")
+        return res.status(400).json({ code: 1004, message: "Invalid device type" });
+    else
+        return res.status(400).json({ code: 1000, message: "Your device information has been recorded" });
+});
+
+router.post("/check_new_version", (req, res) => {
+    var { token, lastUpdate } = req.body;
+    if (!token || !lastUpdate)
+        return res.status(400).json({ code: 1004, message: "Please enter all fields" });
     jwt.verify(token, config.get('jwtSecret'), (err, user) => {
 
       // not valid token
@@ -253,13 +275,30 @@ router.post("/set_devtoken", (req, res) => {
           return res.json({ code: 1004, message: "Your account is blocked" });
       }
     })
-    if (!token || !devtype || !devtoken)
-        return res.status(400).json({ code: 1004, message: "Please enter all fields" });
-    else if (devtype != "Android" && devtype != "IOS")
-        return res.status(400).json({ code: 1004, message: "Invalid device type" });
-    else
-        return res.status(400).json({ code: 1000, message: "Your device information has been recorded" });
+    if (lastUpdate != currentVersion){
+        return res.json({
+            code: 1000,
+            message: "You need to update your browser",
+            data: {
+                version: currentVersion,
+                required: 1,
+                url: "updateversion.com"
+            }
+         });
+    }
+    else {
+        return res.json({
+            code: 1000,
+            message: "Your browser is up to date",
+            data: {
+                version: currentVersion,
+                required: 0,
+            }
+         });
+    }
 });
+
+var currentVersion = "1.0";
 
 function random4digit() {
   return Math.floor(Math.random() * 9000) + 1000;
