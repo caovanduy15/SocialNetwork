9dc20d88067ba3b11394d2b8efe4279e2a1bcb3c
thanh.nq <momo2941998@gmail.com>
2020-11-14 08:34:19 +0700

    fix: add createdAt for friend in friends

--- api/models/User.js
+++ api/models/User.js
@@ -30,8 +30,13 @@ const UserSchema = new Schema({
         default: false
     },
     friends: [{
-      type: Schema.Types.ObjectId,
-      ref: 'users',
+      friend: {
+        type: Schema.Types.ObjectId,
+        ref: 'users',
+      },
+      createdAt: {
+        type: Date
+      }
     }],
     friendRequestReceived: [{
       fromUser: {
